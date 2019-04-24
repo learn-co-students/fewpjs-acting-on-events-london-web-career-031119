b407885ed014c9bd71b927c1d55dbab35dfc2be9
oli-gra <oliver.grant@gmail.com>
2019-04-24 12:19:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+let x_cord = parseInt(dodger.style.left)
+
+dodger.style.backgroundColor = "#FF69B4";
+document.addEventListener("keydown", e=>{
+   console.log(e.code)
+
+   if (e.code === 'ArrowLeft') { x_cord -= 5 }
+   if (e.code === 'ArrowRight') { x_cord += 5 }
+   dodger.style.left = x_cord.toString()+'px'
+} )
